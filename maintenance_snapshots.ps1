0436976aa968d9959a06095d4e8b433a473e56f1
John T Skarbek <john.skarbek@nextcentury.com>
2013-09-17 12:04:56 -0400

    Utilize array instead of string when passing in multiple vms

--- maintenance_snapshots.ps1
+++ maintenance_snapshots.ps1
@@ -2,7 +2,7 @@
     [string]$VMHost = $(throw "Please specify VMHost"),
     [switch]$create,
     [switch]$remove,
-    [string]$VMs,
+    [array]$VMs,
     [string]$resourcepool
 )
 
@@ -13,14 +13,16 @@ $username = [Environment]::UserName
 # Add PowerCLI cmdlets.
 Add-PSSnapin VMware.VimAutomation.Core 
 
-#Connect to vCenter
+# Connect to vCenter
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore
 Connect-VIServer -Server $VMHost
 
 # Get list of VMs
 if ($VMs) {
-    $guests = $VMs | Get-VM
-   }
+    foreach ( $i in $VMs) {
+        $guests += Get-VM -Name $i
+    }
+}
 elseif (( $resourcepool) -and !($VMs)) {
     $guests = get-resourcepool $resourcepool | Get-VM | where { $_.PowerState -eq "PoweredOn" }
 }
